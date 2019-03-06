7f94c8f1bfb9adc205c7cc9f986d894862a17098
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-07 00:36:50 +0900

    [issue #4] ウィンドウサイズを入れる構造体の作成

---
+++ GameFramework/RectSize/RectSize.h
@@ -0,0 +1,25 @@
+﻿//----------------------------------------------------------
+// <filename>RectSize.h</filename>
+//----------------------------------------------------------
+
+#ifndef RECT_SIZE_H
+#define RECT_SIZE_H
+
+/// <summary>
+/// 四角形の長さを保存するための構造体
+/// </summary>
+struct RectSize
+{
+public:	
+	/// <summary>
+	/// x成分の長さ
+	/// </summary>
+	float width;
+	
+	/// <summary>
+	/// y成分の長さ
+	/// </summary>
+	float height;
+};
+
+#endif //! RECT_SIZE_H
