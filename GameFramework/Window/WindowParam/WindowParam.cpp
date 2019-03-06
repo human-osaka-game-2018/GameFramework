7fb33876ac57225b079f46503881e39b80d64948
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-06 21:56:16 +0900

    [issue #4] ウィンドウを作成するのに必要なデータをまとめたクラスの作成

---
+++ GameFramework/Window/WindowParam/WindowParam.cpp
@@ -0,0 +1,13 @@
+﻿//----------------------------------------------------------
+// <filename>WindowParam.cpp</filename>
+//----------------------------------------------------------
+
+#include "WindowParam.h"
+
+#include <Windows.h>
+
+RectSize WindowParam::m_windowSize = { 1280.0f, 720.0f };
+
+HINSTANCE WindowParam::m_hInstance = nullptr;
+
+HWND WindowParam::m_hWnd = nullptr;
