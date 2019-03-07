20034b058dcd284e87bcc4073a704ab76e385d7e
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-07 16:31:21 +0900

    [issue #8] RectSizeの機能追加

--- GameFramework/RectSize/RectSize.h
+++ GameFramework/RectSize/RectSize.h
@@ -11,15 +11,83 @@
 struct RectSize
 {
 public:	
+	RectSize(float width, float height)
+	{
+		m_width = width;
+		m_height = height;
+	}
+	
+	/// <summary>
+	/// 引数のRECT構造体から矩形のサイズを作成する
+	/// </summary>
+	/// <param name="rect">作成するときに扱うRECT構造体</param>
+	RectSize(const RECT& rect)
+	{
+		*this = rect;
+	}
+
 	/// <summary>
 	/// x成分の長さ
 	/// </summary>
-	float width;
+	float m_width;
 	
 	/// <summary>
 	/// y成分の長さ
 	/// </summary>
-	float height;
+	float m_height;
+	
+	/// <summary>
+	/// RECT構造体から矩形のサイズを作成する
+	/// </summary>
+	/// <param name="rhs">作成するときに扱うRECT構造体</param>
+	/// <returns>自身の参照</returns>
+	RectSize& operator = (const RECT& rhs)
+	{
+		m_width = static_cast<float>(rhs.right - rhs.left);
+		m_height = static_cast<float>(rhs.bottom - rhs.top);
+
+		return *this;
+	}
+	
+	/// <summary>
+	/// 引数のサイズと自身のサイズを足しその値を返す
+	/// </summary>
+	/// <param name="rhs">自身と足すRectSize構造体</param>
+	/// <returns>足した結果のRectSize構造体</returns>
+	const RectSize operator + (const RectSize& rhs) const
+	{
+		return RectSize(m_width + rhs.m_width, m_height + rhs.m_height);
+	}
+
+	/// <summary>
+	/// 引数のサイズと自身のサイズを引きその値を返す
+	/// </summary>
+	/// <param name="rhs">自身と引くRectSize構造体</param>
+	/// <returns>引いた結果のRectSize構造体</returns>
+	const RectSize operator - (const RectSize& rhs) const
+	{
+		return RectSize(m_width - rhs.m_width, m_height - rhs.m_height);
+	}
+	
+	/// <summary>
+	/// 引数の値でメンバを割る
+	/// </summary>
+	/// <param name="rhs">割る際の値</param>
+	/// <returns>割った後の結果のRectSize構造体</returns>
+	const RectSize operator / (float rhs) const
+	{
+		return RectSize(m_width / rhs, m_height / rhs);
+	}
+	
+	/// <summary>
+	/// 引数の値でメンバを掛ける
+	/// </summary>
+	/// <param name="rhs">掛ける際の値</param>
+	/// <returns>掛けた後の結果のRectSize構造体</returns>
+	const RectSize operator * (float rhs) const
+	{
+		return RectSize(m_width * rhs, m_height * rhs);
+	}
 };
 
 #endif //! RECT_SIZE_H
