52d8f8cf434cacf20689bcc5bf70fec2d774a861
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-09 00:19:16 +0900

    [issue #14] DirectXDeviceの基底クラスの作成

---
+++ GameFramework/GameFramework/DirectXParam/DirectXDevice/DirectXDevice.h
@@ -0,0 +1,57 @@
+﻿//----------------------------------------------------------
+// <filename>DirectXDevice.h</filename>
+//----------------------------------------------------------
+
+#ifndef DIRECT_X_DEVICE_H
+#define DIRECT_X_DEVICE_H
+
+#include <Windows.h>
+
+#include <d3dx9.h>
+
+/// <summary>
+/// 基礎構築をに関するものをまとめた名前空間
+/// </summary>
+namespace BaseFramework
+{
+	#ifdef DIRECT_X_VERSION_11
+	using LPDIRECT3D = ID3D11Device*;
+	#else
+	using LPDIRECT3D = LPDIRECT3D9;
+	#endif //! DIRECT_X_VERSION_11
+		
+	/// <summary>
+	/// DirectXのデバイスクラス
+	/// </summary>
+	class DirectXDevice
+	{
+	public:
+		DirectXDevice() {};
+
+		virtual ~DirectXDevice()
+		{
+			m_pDirectXDevice->Release();
+			delete m_pDirectXDevice;
+		}
+
+		inline void Get(LPDIRECT3D* ppDirectXDevice) const
+		{
+			*ppDirectXDevice = m_pDirectXDevice;
+		}
+
+	protected:
+		/// <summary>
+		/// DirectXのデバイスを作成する
+		/// </summary>
+		virtual void Create() = 0;
+
+		LPDIRECT3D m_pDirectXDevice = nullptr;
+
+	private:
+		DirectXDevice(const DirectXDevice& rhs) = delete;
+
+		DirectXDevice& operator=(const DirectXDevice& rhs) = delete;
+	};
+}
+
+#endif //! DIRECT_X_DEVICE_H
