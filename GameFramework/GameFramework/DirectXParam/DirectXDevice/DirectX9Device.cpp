60f028794bc343d4409c01925df30f375772b67e
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-09 02:38:41 +0900

    [issue #14] DirectX9バージョンの作成

---
+++ GameFramework/GameFramework/DirectXParam/DirectXDevice/DirectX9Device.cpp
@@ -0,0 +1,31 @@
+﻿//----------------------------------------------------------
+// <filename>DirectXDevice.cpp</filename>
+//----------------------------------------------------------
+
+#include "DirectX9Device.h"
+
+#include <Windows.h>
+#include <tchar.h>
+
+#include <d3dx9.h>
+
+#include "DirectXDevice.h"
+
+namespace BaseFramework
+{
+	void DirectX9Device::Create()
+	{
+		if (m_pDirectXDevice = Direct3DCreate9(D3D_SDK_VERSION))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("アプリケーションを終了します"),
+			_T("DirectXDeviceの作成に失敗しました"),
+			MB_OK | MB_ICONERROR);
+
+		PostQuitMessage(0);
+	}
+}
