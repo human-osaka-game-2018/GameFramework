7dc3c2d42ccbc3c864e3aab9aefe33709de4e35e
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-09 20:21:42 +0900

    [issue #19] ファクトリの機能追加
    -  ファクトリの機能修正

--- GameFramework/GameFramework/GameFrameworkFactory/GameFrameworkFactory.h
+++ GameFramework/GameFramework/GameFrameworkFactory/GameFrameworkFactory.h
@@ -9,6 +9,8 @@
 #include "DirectXDevice/DirectX9Device.h"
 #include "DirectXGraphicDevice/DirectXGraphicDevice.h"
 #include "DirectXGraphicDevice/DirectX9GraphicDevice.h"
+#include "DirectXInputDevice/DirectXInputDevice.h"
+#include "DirectXInputDevice/DirectX8InputDevice.h"
 
 /// <summary>
 /// 基礎構築をに関するものをまとめた名前空間
@@ -19,27 +21,37 @@ namespace BaseFramework
 	{
 	public:
 		template<typename T>
-		static void Create(T* pT)
+		static void Create(T** ppT)
 		{
 			pT = new T();
 		}
 
 		template<>
-		static void Create<DirectXDevice>(DirectXDevice* pDirectXDevice)
+		static void Create<DirectXDevice>(DirectXDevice** ppDirectXDevice)
 		{
 			#ifdef DIRECT_X_VERSION_11
-			pDirectXDevice = nullptr;
+			*ppDirectXDevice = nullptr;
 			#else
-			pDirectXDevice = new DirectX9Device();
+			*ppDirectXDevice = new DirectX9Device();
 			#endif
 		}
 
-		static void Create(DirectXGraphicDevice* pDirectXGraphicDevice, DirectXDevice* pDirectXDevice)
+		static void Create(DirectXGraphicDevice** ppDirectXGraphicDevice, DirectXDevice* pDirectXDevice)
 		{
 			#ifdef DIRECT_X_VERSION_11
-			pDirectXGraphicDevice = nullptr;
+			ppDirectXDevice = nullptr;
 			#else
-			pDirectXGraphicDevice = new DirectX9GraphicDevice(pDirectXDevice);
+			*ppDirectXGraphicDevice = new DirectX9GraphicDevice(pDirectXDevice);
+			#endif
+		}
+
+		template<>
+		static void Create<DirectXInputDevice>(DirectXInputDevice** ppDirectXInputDevice)
+		{
+			#ifdef DIRECT_X_VERSION_11
+			pDirectXInputDevice = nullptr;
+			#else
+			*ppDirectXInputDevice = new DirectX8InputDevice();
 			#endif
 		}
 
