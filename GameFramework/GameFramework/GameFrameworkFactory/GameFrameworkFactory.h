8463a10ec537bb169ef7e0f8e69411337845b8ea
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-09 12:18:22 +0900

    [issue #14] ファクトリーの作成

---
+++ GameFramework/GameFramework/GameFrameworkFactory/GameFrameworkFactory.h
@@ -0,0 +1,45 @@
+﻿//----------------------------------------------------------
+// <filename>GameFrameworkFactory.h</filename>
+//----------------------------------------------------------
+
+#ifndef GAME_FRAMEWORK_FACTORY_H
+#define GAME_FRAMEWORK_FACTORY_H
+
+#include "DirectXDevice/DirectXDevice.h"
+#include "DirectXDevice/DirectX9Device.h"
+
+/// <summary>
+/// 基礎構築をに関するものをまとめた名前空間
+/// </summary>
+namespace BaseFramework
+{
+	class GameFrameFactory
+	{
+	public:
+		template<typename T>
+		static void Create(T* pT)
+		{
+			pT = new T();
+		}
+
+		template<>
+		static void Create<DirectXDevice>(DirectXDevice* pDirectXDevice)
+		{
+			#ifdef DIRECT_X_VERSION_11
+			pDirectXDevice = nullptr;
+			#else
+			pDirectXDevice = new DirectX9Device();
+			#endif
+		}
+
+	private:
+		GameFrameFactory() {};
+		GameFrameFactory(const GameFrameFactory& rhs) = delete;
+
+		~GameFrameFactory() {};
+
+		GameFrameFactory& operator=(const GameFrameFactory& rhs) = delete;
+	};
+}
+
+#endif //! GAME_FRAMEWORK_FACTORY_H
