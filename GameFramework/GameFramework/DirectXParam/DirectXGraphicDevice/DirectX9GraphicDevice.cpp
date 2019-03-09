364f42355ef2b97c4854aaccfeec9b1cc41a77ac
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-09 17:18:27 +0900

    [issue #16] DirectX9バージョンを作成

---
+++ GameFramework/GameFramework/DirectXParam/DirectXGraphicDevice/DirectX9GraphicDevice.cpp
@@ -0,0 +1,84 @@
+﻿//----------------------------------------------------------
+// <filename>DirectX9GraphicDevice.cpp</filename>
+//----------------------------------------------------------
+
+#include "DirectX9GraphicDevice.h"
+
+#include <Windows.h>
+#include <tchar.h>
+
+#include <d3dx9.h>
+
+#include "RectSize.h"
+#include "WindowParam/WindowParam.h"
+#include "DirectXDevice/DirectXDevice.h"
+#include "DirectXGraphicDevice.h"
+
+namespace BaseFramework
+{
+	void DirectX9GraphicDevice::DirectXPresentParam::FormatUnionParam(D3DPRESENT_PARAMETERS* pD3DPRESENT_PARAMETERS)
+	{
+		ZeroMemory(pD3DPRESENT_PARAMETERS, sizeof(D3DPRESENT_PARAMETERS));
+
+		RectSize windowSize;
+		WindowParam::GetWindowSize(&windowSize);
+		pD3DPRESENT_PARAMETERS->BackBufferWidth = static_cast<UINT>(windowSize.m_width);
+		pD3DPRESENT_PARAMETERS->BackBufferHeight = static_cast<UINT>(windowSize.m_height);
+
+		pD3DPRESENT_PARAMETERS->BackBufferFormat = D3DFMT_X8R8G8B8;
+		pD3DPRESENT_PARAMETERS->BackBufferCount = 1;
+
+		pD3DPRESENT_PARAMETERS->SwapEffect = D3DSWAPEFFECT_DISCARD;
+
+		pD3DPRESENT_PARAMETERS->EnableAutoDepthStencil = true;
+	}
+
+	void DirectX9GraphicDevice::Create(DirectXDevice* pDirectXDevice)
+	{
+		HWND hWnd = nullptr;
+		WindowParam::GetWindowHandle(&hWnd);
+
+		LPDIRECT3D pDirect3D = nullptr;
+		pDirectXDevice->Get(&pDirect3D);
+
+		D3DPRESENT_PARAMETERS directXPresentParams;
+		DirectXPresentParam::GetWindowModeParam(&directXPresentParams);
+
+		if (SUCCEEDED(pDirect3D->CreateDevice(
+
+			//描画をハードウェアに依存させる 軽い
+			D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,
+			hWnd,
+			D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_MULTITHREADED,
+			&directXPresentParams,
+			&m_pDirectXGraphicDevice)))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("REFモードで再試行します"),
+			_T("HALモードでDirectXのグラフィックデバイスを作成できませんでした"),
+			MB_OK);
+
+		if (SUCCEEDED(pDirect3D->CreateDevice(
+			//! 描画をソフトウェアに依存させる 重い
+			D3DADAPTER_DEFAULT, D3DDEVTYPE_REF,
+			hWnd,
+			D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_MULTITHREADED,
+			&directXPresentParams,
+			&m_pDirectXGraphicDevice)))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("アプリケーションを終了します"),
+			_T("DirectXのグラフィックデバイスを作成できませんでした"),
+			MB_OK | MB_ICONERROR);
+
+		PostQuitMessage(0);
+	}
+}
---
+++ GameFramework/GameFramework/DirectXParam/DirectXGraphicDevice/DirectX9GraphicDevice.h
@@ -0,0 +1,103 @@
+﻿//----------------------------------------------------------
+// <filename>DirectX9GraphicDevice.h</filename>
+//----------------------------------------------------------
+
+#ifndef DIRECT_X_9_GRAPHIC_DEVICE_H
+#define DIRECT_X_9_GRAPHIC_DEVICE_H
+
+#include <Windows.h>
+#include <tchar.h>
+
+#include <d3dx9.h>
+
+#include "RectSize.h"
+#include "WindowParam/WindowParam.h"
+#include "DirectXDevice/DirectXDevice.h"
+#include "DirectXGraphicDevice.h"
+
+/// <summary>
+/// 基礎構築をに関するものをまとめた名前空間
+/// </summary>
+namespace BaseFramework
+{
+	/// <summary>
+	/// DirectXのグラフィックデバイスクラス
+	/// </summary>
+	class DirectX9GraphicDevice :public DirectXGraphicDevice
+	{
+	public:
+		DirectX9GraphicDevice(DirectXDevice* pDirectXDevice)
+		{
+			Create(pDirectXDevice);
+		}
+
+		virtual ~DirectX9GraphicDevice()
+		{
+			m_pDirectXGraphicDevice->Release();
+			delete m_pDirectXGraphicDevice;
+		}
+
+		inline void Get(LPDIRECT3DDEVICE* ppDirectXDevice) const
+		{
+			*ppDirectXDevice = m_pDirectXGraphicDevice;
+		}
+
+	private:
+		/// <summary>
+		/// DirectXのバックバッファ等の情報をまとめたクラス
+		/// </summary>
+		class DirectXPresentParam
+		{
+		public:			
+			/// <summary>
+			/// ウィンドウモード時のデータを取得
+			/// </summary>
+			/// <param name="pD3DPRESENT_PARAMETERS">取得した値を入れる構造体のポインタ</param>
+			static inline void GetWindowModeParam(D3DPRESENT_PARAMETERS* pD3DPRESENT_PARAMETERS)
+			{
+				FormatUnionParam(pD3DPRESENT_PARAMETERS);
+
+				pD3DPRESENT_PARAMETERS->Windowed = true;
+			}
+			
+			/// <summary>
+			/// フルスクリーンモード時のデータを取得
+			/// </summary>
+			/// <param name="pD3DPRESENT_PARAMETERS">取得した値を入れる構造体のポインタ</param>
+			static inline void GetFullScreenParam(D3DPRESENT_PARAMETERS* pD3DPRESENT_PARAMETERS)
+			{
+				FormatUnionParam(pD3DPRESENT_PARAMETERS);
+
+				pD3DPRESENT_PARAMETERS->Windowed = false;
+
+				pD3DPRESENT_PARAMETERS->PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
+
+				HWND hWnd = nullptr;
+				WindowParam::GetWindowHandle(&hWnd);
+				pD3DPRESENT_PARAMETERS->hDeviceWindow = hWnd;
+			}
+
+		private:
+			DirectXPresentParam() {};
+			DirectXPresentParam(const DirectXPresentParam& rhs) = delete;
+
+			~DirectXPresentParam() {};
+
+			DirectXPresentParam& operator=(const DirectXPresentParam& rhs) = delete;
+
+			static void FormatUnionParam(D3DPRESENT_PARAMETERS* pD3DPRESENT_PARAMETERS);
+		};
+
+		DirectX9GraphicDevice(const DirectX9GraphicDevice& rhs) = delete;
+
+		DirectX9GraphicDevice& operator=(const DirectX9GraphicDevice& rhs) = delete;
+
+		/// <summary>
+		/// DirectXのグラフィックデバイスを作成する
+		/// </summary>
+		/// <param name="pDirectXDevice">DirectXのデバイスクラスのポインタ</param>
+		void Create(DirectXDevice* pDirectXDevice);
+	};
+}
+
+#endif //! DIRECT_X_GRAPHIC_DEVICE_H
