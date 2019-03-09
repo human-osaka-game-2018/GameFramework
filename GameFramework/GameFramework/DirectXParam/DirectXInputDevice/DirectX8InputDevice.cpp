d14217a20445d38af0aa84c0dc5b1b3715861de1
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-09 20:16:31 +0900

    [issue #19] DirectX8バージョンの作成

---
+++ GameFramework/GameFramework/DirectXParam/DirectXInputDevice/DirectX8InputDevice.cpp
@@ -0,0 +1,39 @@
+﻿//----------------------------------------------------------
+// <filename>DirectXInput8Device.cpp</filename>
+//----------------------------------------------------------
+
+#include "DirectX8InputDevice.h"
+
+#include <Windows.h>
+#include <tchar.h>
+
+#include <dinput.h>
+
+#include "WindowParam/WindowParam.h"
+#include "DirectXInputDevice.h"
+
+namespace BaseFramework
+{
+	void DirectX8InputDevice::Create()
+	{
+		HINSTANCE hInstance = nullptr;
+		WindowParam::GetInstanceHandle(&hInstance);
+
+		if (SUCCEEDED(DirectInput8Create(
+			hInstance,
+			DIRECTINPUT_VERSION, IID_IDirectInput8,
+			(void**)&m_pDirectXInputDevice,
+			NULL)))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("アプリケーションを終了します"),
+			_T("DirectXの入力デバイスを作成できませんでした"),
+			MB_OK | MB_ICONERROR);
+
+		PostQuitMessage(0);
+	}
+}
