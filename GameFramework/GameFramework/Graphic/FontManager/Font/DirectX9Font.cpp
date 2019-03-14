2cb004c062311444eb558f0801ef446d8472e427
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-15 02:58:56 +0900

    [issue #69] フォントクラスの作成

---
+++ GameFramework/GameFramework/Graphic/FontManager/Font/DirectX9Font.cpp
@@ -0,0 +1,33 @@
+﻿//----------------------------------------------------------
+// <filename>DirectX9Font.cpp</filename>
+//----------------------------------------------------------
+
+#include "DirectX9Font.h"
+
+namespace gameframework
+{
+	#ifdef DIRECT_X_VERSOIN_9
+
+	void DirectX9Font::Reset(const RectSize& scale, const TCHAR* pFontName, UINT thickness)
+	{
+		m_fontSize = scale;
+
+		Release();
+
+		D3DXCreateFont(
+			m_pDirectXGraphicDevice,
+			static_cast<int>(scale.m_width), static_cast<int>(scale.m_height),
+			thickness,
+			0,
+			0,
+			//日本語を表示するのでキャラセットを固定
+			SHIFTJIS_CHARSET,
+			0,
+			0,
+			0,
+			pFontName,
+			&m_pFont);
+	}
+
+	#endif //DIRECT_X_VERSOIN_9
+}
