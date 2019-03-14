aa9ff9b99efb93b319f9a5a7834acb788a4efc82
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-14 23:58:53 +0900

    [issue #39] テクスチャクラスの作成

---
+++ GameFramework/GameFramework/Graphic/TextureManager/Texture/DirectX9Texture.cpp
@@ -0,0 +1,30 @@
+﻿#include "DirectX9Texture.h"
+
+/// <summary>
+/// 基礎構築に関するものをまとめた名前空間
+/// </summary>
+namespace gameframework
+{
+	#ifdef DIRECT_X_VERSOIN_9
+
+	void DirectX9Texture::Reset(const TCHAR* pTexturePath)
+	{
+		Release();
+
+		LPDIRECT3DDEVICE pDirectXGraphicDevice = nullptr;
+		DirectXParam::GetDirectXGraphicDevice()->Get(&pDirectXGraphicDevice);
+
+		D3DXCreateTextureFromFile(
+			pDirectXGraphicDevice,
+			pTexturePath,
+			&m_pTexture);
+
+		D3DSURFACE_DESC desc;
+		m_pTexture->GetLevelDesc(0, &desc);
+
+		m_textureSize.m_width  = static_cast<float>(desc.Width);
+		m_textureSize.m_height = static_cast<float>(desc.Height);
+	}
+
+	#endif //DIRECT_X_VERSOIN_9
+}
