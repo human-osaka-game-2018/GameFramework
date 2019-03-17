bd51f3d6e6dc83d5f705c7e47051b5b14a889389
taishi-matsumura <taishi0706@gmail.com>
2019-03-17 18:39:33 +0900

    :white_check_mark: Algorithmクラスのテストを追加(#105)

---
+++ GameFrameworkTest/Algorithm/AlgorithmTest.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+
+#include "Algorithm.h"
+
+using namespace gameframework;
+
+TEST(AlgorithmTest, SwitchMinBetweenMax) {
+	// Arrange
+	int cycleVal = 5;
+	float switchingMin = 0.5f;
+	float switchingMax = 2.0f;
+	std::vector<float> retVals;
+
+	// Act
+	for (int val = 0; val <= cycleVal; ++val) {
+		float ret = algorithm::SwitchMinBetweenMax(val, cycleVal, switchingMin, switchingMax);
+		retVals.push_back(ret);
+	}
+
+	// Assert
+	EXPECT_FLOAT_EQ(switchingMin, retVals[0]);
+	EXPECT_FLOAT_EQ(switchingMax, retVals[5]);
+}
