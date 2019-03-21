cc08fe41d4b3bb13edf32929333840f2c574805e
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-21 22:08:55 +0900

    [issue #129] ✨ 当たり判定関数をまとめた名前空間の作成

---
+++ GameFramework/GameFramework/Collision/Collision.cpp
@@ -0,0 +1,110 @@
+﻿#include "Collision.h"
+
+#include <d3dx9.h>
+
+namespace gameframework
+{
+	namespace collision
+	{
+		void CreateSideVectors(D3DXVECTOR2* pSideVecs, const CustomVertex* pVertices)
+		{
+			for (int i = 0; i < CustomVertex::RECT_VERTICES_NUM; ++i)
+			{
+				//一つ先の頂点とのベクトルを作成
+				D3DXVECTOR3 vec3Tmp = pVertices[(i + 1) % 4].m_pos - pVertices[i].m_pos;
+
+				pSideVecs[i] = { vec3Tmp.x, vec3Tmp.y };
+			}
+		};
+
+		void CreateVectorsVertexToPos(D3DXVECTOR2* pVecsVToP, const CustomVertex* pVertices, const D3DXVECTOR3& pos)
+		{
+			for (int i = 0; i < CustomVertex::RECT_VERTICES_NUM; ++i)
+			{
+				D3DXVECTOR3 vec3Tmp = pos - pVertices[i].m_pos;
+
+				pVecsVToP[i] = { vec3Tmp.x, vec3Tmp.y };
+			}
+		};
+
+		bool IsInner(const D3DXVECTOR2* pSideVecs, const D3DXVECTOR2 * pVecsVToP)
+		{
+			for (int i = 0; i < CustomVertex::RECT_VERTICES_NUM; ++i)
+			{
+				//角度が正ならば+で帰ってくる
+				float degree = algorithm::D3DXVec2CalcDegree(pSideVecs[i], pVecsVToP[i]);
+
+				//負ならば辺の左側に点があるということになるので矩形の中に点は存在していない
+				if (degree < 0) return false;
+			}
+
+			//4辺全てから見て点が右側にあるということはその点が矩形に内包されているということになる
+			return true;
+		};
+
+		bool Collides(const CustomVertex* pBaseVertices, const CustomVertex* pVertices)
+		{
+			D3DXVECTOR2 sideVecs[CustomVertex::RECT_VERTICES_NUM];
+			CreateSideVectors(sideVecs, pBaseVertices);
+
+			for (int i = 0; i < CustomVertex::RECT_VERTICES_NUM; ++i)
+			{
+				D3DXVECTOR2 vecsVToP[CustomVertex::RECT_VERTICES_NUM];
+				CreateVectorsVertexToPos(vecsVToP, pBaseVertices, pVertices[i].m_pos);
+
+				if (IsInner(sideVecs, vecsVToP)) return true;
+			}
+
+			//どの頂点も中になかった
+			return false;
+		};
+
+		bool Collides(Vertices& rectA, Vertices& rectB)
+		{
+			if (Collides(rectA.GetCustomVertex(), rectB.GetCustomVertex()) ||
+				Collides(rectB.GetCustomVertex(), rectA.GetCustomVertex()))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		bool IsInner(Vertices& rect, D3DXVECTOR3& pos)
+		{
+			D3DXVECTOR2 sideVecs[CustomVertex::RECT_VERTICES_NUM];
+			CreateSideVectors(sideVecs, rect.GetCustomVertex());
+
+			D3DXVECTOR2 vecVToP[CustomVertex::RECT_VERTICES_NUM];
+			CreateVectorsVertexToPos(vecVToP, rect.GetCustomVertex(), pos);
+
+			return IsInner(sideVecs, vecVToP);
+		}
+
+		bool Collides(Vertices& circleA, Vertices& circleB)
+		{
+			D3DXVECTOR3 vecCenterToCenter = circleA.GetCenter() - circleB.GetCenter();
+
+			if (D3DXVec3Length(&vecCenterToCenter) >
+				(circleA.GetSizeForRender().m_width + circleB.GetSizeForRender().m_width) * 0.5f)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		bool Collides(Vertices& circle, D3DXVECTOR3& Pos)
+		{
+			D3DXVECTOR3 vecPosToCenter = circle.GetCenter() - Pos;
+
+			if (D3DXVec3Length(&vecPosToCenter) >
+				circle.GetSizeForRender().m_width * 0.5f)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ GameFramework/GameFramework/Collision/Collision.h
@@ -0,0 +1,81 @@
+﻿#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "RectSize.h"
+#include "Degree.h"
+#include "Vertices.h"
+#include "algorithm.h"
+
+/// <summary>
+/// 基礎構築に関するものをまとめた名前空間
+/// </summary>
+namespace gameframework
+{
+	namespace collision
+	{
+		/// <summary>
+		/// 矩形の辺のベクトルを作成する
+		/// </summary>
+		/// <param name="pSideVecs">作成したベクトルを入れる構造体の先頭アドレス</param>
+		/// <param name="pVertices">頂点情報構造体の先頭アドレス</param>
+		void CreateSideVectors(D3DXVECTOR2* pSideVecs, const CustomVertex* pVertices);
+
+		/// <summary>
+		/// 矩形の4頂点から引数の点へのベクトルを作成する
+		/// </summary>
+		/// <param name="pVecsVToP">作成したベクトルを入れる構造体の先頭アドレス</param>
+		/// <param name="pVertices">頂点情報構造体の先頭アドレス</param>
+		/// <param name="pos">点</param>
+		void CreateVectorsVertexToPos(D3DXVECTOR2* pVecsVToP, const CustomVertex* pVertices, const D3DXVECTOR3& pos);
+
+		/// <summary>
+		/// 矩形の辺のベクトルと矩形の頂点から任意の点へのベクトルを用いその点が矩形の内側にあるかを判断する
+		/// </summary>
+		/// <param name="pSideVecs">矩形の辺のベクトルの先頭アドレス</param>
+		/// <param name="pVecsVToP">矩形の頂点から任意の点へのベクトルのアドレス</param>
+		/// <returns>中にあればtrue</returns>
+		bool IsInner(const D3DXVECTOR2* pSideVecs, const D3DXVECTOR2 * pVecsVToP);
+
+		/// <summary>
+		/// 既定となる矩形から見てもう一つの矩形の頂点が内包されているかを判定する
+		/// </summary>
+		/// <param name="pBaseVertices">既定となる頂点情報構造体の先頭アドレス</param>
+		/// <param name="pVertices">頂点情報構造体の先頭アドレス</param>
+		/// <returns>中にあればtrue</returns>
+		bool Collides(const CustomVertex* pBaseVertices, const CustomVertex* pVertices);
+
+		/// <summary>
+		/// 矩形と矩形の当たり判定チェック
+		/// </summary>
+		/// <param name="rectA">矩形クラス</param>
+		/// <param name="rectB">矩形クラス</param>
+		/// <returns>当たっていたらtrue</returns>
+		bool Collides(Vertices& rectA, Vertices& rectB);
+
+		/// <summary>
+		/// 矩形と点の当たり判定チェック
+		/// </summary>
+		/// <param name="rect">矩形クラス</param>
+		/// <param name="pos">点</param>
+		/// <returns>当たっていたらtrue</returns>
+		bool IsInner(Vertices& rect, D3DXVECTOR3& pos);
+
+		/// <summary>
+		/// 円と円の当たり判定チェック
+		/// </summary>
+		/// <param name="circleA">矩形クラス</param>
+		/// <param name="circleB">矩形クラス</param>
+		/// <returns>当たっていたらtrue</returns>
+		bool Collides(Vertices& circleA, Vertices& circleB);
+
+		/// <summary>
+		/// 円と点の当たり判定チェック
+		/// </summary>
+		/// <param name="circle">矩形クラス</param>
+		/// <param name="Pos">点</param>
+		/// <returns>当たっていたらtrue</returns>
+		bool Collides(Vertices& circle, D3DXVECTOR3& Pos);
+	}
+}
+
+#endif //!COLLISION_H
