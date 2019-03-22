208463de91a0f52ae0f55899f5d8c7c6cb411a9d
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-22 22:36:35 +0900

    [issue #129] 当たり判定で使用する矩形に関係するベクトルのファーストクラスコレクション

---
+++ GameFramework/GameFramework/Collision/RectVectors.cpp
@@ -0,0 +1,29 @@
+﻿#include "RectVectors.h"
+
+namespace gameframework
+{
+	namespace collision
+	{
+		RectVectors::~RectVectors()
+		{
+
+		}
+
+		void RectVectors::GetDegrees(RectDegrees* pRectDegrees, const RectVectors* pRectVectors)const
+		{
+			const D3DXVECTOR2* pRECT_VECS = pRectVectors->Get();
+
+			for (auto& vec : m_vectors)
+			{
+				int index = static_cast<int>(&vec - &m_vectors[0]);
+
+				pRectDegrees->m_degrees[index] = algorithm::D3DXVec2CalcDegree(m_vectors[index], pRECT_VECS[index]);
+			}
+		}
+
+		RectVectors::RectVectors()
+		{
+
+		}
+	}
+}
---
+++ GameFramework/GameFramework/Collision/SideVectors.cpp
@@ -0,0 +1,33 @@
+﻿#include "SideVectors.h"
+
+namespace gameframework
+{
+	namespace collision
+	{
+		SideVectors::SideVectors(Vertices& vertices)
+		{
+			Create(vertices);
+		}
+
+		SideVectors::~SideVectors()
+		{
+
+		}
+
+		void SideVectors::Create(Vertices& vertices)
+		{
+			CustomVertices& customVertices = vertices.GetCustomVertex();
+			CustomVertex* pCustomVertices  = customVertices.Get();
+
+			for (auto& sideVec : m_vectors)
+			{
+				int index = static_cast<int>(&sideVec - &m_vectors[0]);
+
+				//一つ先の頂点とのベクトルを作成
+				D3DXVECTOR3 vec3Tmp = pCustomVertices[(index + 1) % 4].m_pos - pCustomVertices[index].m_pos;
+
+				sideVec = { vec3Tmp.x, vec3Tmp.y };
+			}
+		}
+	}
+}
