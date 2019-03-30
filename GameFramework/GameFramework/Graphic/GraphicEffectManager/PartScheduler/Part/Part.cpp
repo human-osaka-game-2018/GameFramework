960da46ea44ff7f1488ec17c8b2a0af9846c7c98
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-31 01:14:30 +0900

    [#131] エフェクトパーツクラスの作成

---
+++ GameFramework/GameFramework/Graphic/GraphicEffectManager/PartScheduler/Part/Part.cpp
@@ -0,0 +1,62 @@
+﻿#include "Part.h"
+
+namespace gameframework
+{
+	ColorBlender* Part::m_pColorBlender = nullptr;
+	TextureManager* Part::m_pTextureManager = nullptr;
+
+	Part::~Part()
+	{
+		Finalize();
+	}
+
+	void Part::SetGraphicHelper(ColorBlender* pColorBlender, TextureManager* pTextureManager)
+	{
+		m_pColorBlender = pColorBlender;
+		m_pTextureManager = pTextureManager;
+	}
+
+	void Part::Render(bool isAdditionBlendMode)
+	{
+		if (isAdditionBlendMode) m_pColorBlender->ToAddtionBlendMode();
+
+		m_pVertices->Render(m_pTextureManager->Get(TEXTURE_KEY));
+		
+		m_pColorBlender->ToDefaultBlendMode();
+	}
+
+	void Part::LoadResource()
+	{
+		m_pTextureManager->Create(TEXTURE_KEY, TEXTURE_KEY);
+	}
+
+	Part::Part(const TCHAR* pTexturePath) :TEXTURE_KEY(pTexturePath)
+	{
+		Initialize();
+	}
+
+	void Part::Initialize()
+	{
+		GameFrameworkFactory::Create(&m_pVertices);
+		LoadResource();
+	}
+
+	void Part::Finalize()
+	{
+		Release();
+	}
+
+	void Part::Release()
+	{
+		delete m_pVertices;
+	}
+
+	void Part::UpdateVertices()
+	{
+		if (m_isEnd) return;
+
+		m_behaviorScheduler.Update(m_pVertices);
+
+		++m_lifeFrame;
+	}
+}
---
+++ GameFramework/GameFramework/Graphic/GraphicEffectManager/PartScheduler/Part/Part.h
@@ -0,0 +1,82 @@
+﻿#ifndef PART_H
+#define PART_H
+
+#include <tchar.h>
+
+#include "Vertices.h"
+#include "Texture/Texture.h"
+#include "TextureManager.h"
+#include "ColorBlender/ColorBlender.h"
+#include "GameFrameworkFactory.h"
+#include "Behavior/Behavior.h"
+#include "BehaviorScheduler.h"
+
+/// <summary>
+/// 基礎構築に関するものをまとめた名前空間
+/// </summary>
+namespace gameframework
+{
+	using BehaviorData = BehaviorScheduler::NodeData;
+
+	/// <summary>
+	/// エフェクトを構成するパーツクラス
+	/// </summary>
+	class Part
+	{
+	public:
+		virtual ~Part();
+
+		/// <remarks>
+		/// Graphicクラスで入れる
+		/// </remarks>
+		static void SetGraphicHelper(ColorBlender* pColorBlender, TextureManager* pTextureManager);
+		
+		/// <summary>
+		/// 矩形を変形させたりする
+		/// </summary>
+		virtual void Update() = 0;
+
+		/// <summary>
+		/// 描画
+		/// </summary>
+		/// <param name="isAdditionBlendMode">加算合成を行うか</param>
+		virtual void Render(bool isAdditionBlendMode);
+
+		inline bool IsEnd()const
+		{
+			return m_isEnd;
+		}
+
+		/// <summary>
+		/// 画像などの読み込み
+		/// </summary>
+		void LoadResource();
+
+	protected:
+		Part(const Part& part) = delete;
+
+		explicit Part(const TCHAR* pTexturePath);
+
+		Part& operator=(const Part& part) = delete;
+
+		void Initialize();
+
+		void Finalize();
+
+		void Release();
+
+		void UpdateVertices();
+
+		int m_lifeFrame = 0;
+		bool m_isEnd = false;
+
+		const TCHAR* TEXTURE_KEY;
+		Vertices* m_pVertices = nullptr;
+
+		static ColorBlender* m_pColorBlender;
+		static TextureManager* m_pTextureManager;
+		BehaviorScheduler m_behaviorScheduler;
+	};
+}
+
+#endif //!PART_H
