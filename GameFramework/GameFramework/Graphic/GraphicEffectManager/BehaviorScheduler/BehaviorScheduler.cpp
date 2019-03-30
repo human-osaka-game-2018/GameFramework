a3feb9a51d3cedeec47611830cd9810f58817b5f
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-30 21:09:51 +0900

    [#131] エフェクト関係の管理クラスの追加

---
+++ GameFramework/GameFramework/Graphic/GraphicEffectManager/BehaviorScheduler/BehaviorScheduler.cpp
@@ -0,0 +1,42 @@
+﻿#include "BehaviorScheduler.h"
+
+namespace gameframework
+{
+	BehaviorScheduler::BehaviorScheduler()
+	{
+
+	}
+
+	BehaviorScheduler::~BehaviorScheduler()
+	{
+	
+	}
+
+	void BehaviorScheduler::Update(Vertices* pVertices)
+	{
+		UpdateCanRunning();
+
+		for (auto& behaviorData : m_nodeDatas)
+		{
+			if (!behaviorData.m_canRunning) continue;
+
+			behaviorData.m_pT->Impart(pVertices);
+		}
+	}
+
+	bool BehaviorScheduler::IsParentNodeEnded(NodeData& nodeData)
+	{
+		//親ノードが終了しているかチェック
+		int index = nodeData.m_parentNodeIndex;
+
+		//親ノードがある
+		if (index != NodeData::DEFAULT_START)
+		{
+			//親ノードが終了していない
+			if (!m_nodeDatas[index].m_pT->IsEnd() &&
+				m_nodeDatas[index].m_runableRemainingFrame != 0) return false;
+		}
+
+		return true;
+	}
+}
--- GameFramework/GameFramework/Graphic/GraphicEffectManager/GraphicEffectManager.h
+++ GameFramework/GameFramework/Graphic/GraphicEffectManager/GraphicEffectManager.h
@@ -1,10 +1,55 @@
-﻿//----------------------------------------------------------
-// <filename></filename>
-//----------------------------------------------------------
+﻿#ifndef GRAPHIC_EFFECT_MANAGER_H
+#define GRAPHIC_EFFECT_MANAGER_H
 
-#ifndef 
-#define 
+#include <vector>
 
+#include "GraphicEffect/GraphicEffect.h"
 
+/// <summary>
+/// 基礎構築に関するものをまとめた名前空間
+/// </summary>
+namespace gameframework
+{
+	/// <summary>
+	/// グラフィックエフェクト管理クラス
+	/// </summary>
+	class GraphicEffectManager
+	{
+	public:
+		GraphicEffectManager();
 
-#endif //! 
+		~GraphicEffectManager();
+
+		/// <summary>
+		/// エフェクトの登録
+		/// </summary>
+		/// <param name="pGraphicEffect">登録するエフェクトのポインタ</param>
+		void Register(GraphicEffect* pGraphicEffect);
+
+		/// <summary>
+		/// エフェクトの全開放
+		/// </summary>
+		void ReleaseAll();
+
+		/// <summary>
+		/// 全てのエフェクトを更新する
+		/// </summary>
+		void Update();
+
+		/// <summary>
+		/// 全てのエフェクトを描画する
+		/// </summary>
+		void Render();
+
+	protected:
+		GraphicEffectManager(const GraphicEffectManager& graphicEffectManager) = delete;
+
+		GraphicEffectManager& operator=(const GraphicEffectManager& graphicEffectManager) = delete;
+
+		void ReleaseEndEffect();
+
+		std::vector<GraphicEffect*> m_pGraphicEffects;
+	};
+}
+
+#endif //!GRAPHIC_EFFECT_MANAGER_H
