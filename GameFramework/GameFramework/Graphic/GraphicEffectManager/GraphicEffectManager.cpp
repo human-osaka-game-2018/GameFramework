6f8fb70fc37f504a2a95e5ac06e14556efe7ff35
Harutaka-Tsujino <glaca986@outlook.jp>
2019-03-30 21:09:51 +0900

    [#131] エフェクト関係の管理クラスの追加

--- GameFramework/GameFramework/Graphic/GraphicEffectManager/GraphicEffectManager.cpp
+++ GameFramework/GameFramework/Graphic/GraphicEffectManager/GraphicEffectManager.cpp
@@ -1,5 +1,71 @@
-﻿//----------------------------------------------------------
-// <filename></filename>
-//----------------------------------------------------------
+﻿#include "GraphicEffectManager.h"
 
+namespace gameframework
+{
+	GraphicEffectManager::GraphicEffectManager()
+	{
 
+	}
+
+	GraphicEffectManager::~GraphicEffectManager()
+	{
+
+	}
+
+	void GraphicEffectManager::Register(GraphicEffect* pGraphicEffect)
+	{
+		m_pGraphicEffects.push_back(pGraphicEffect);
+	}
+
+	void GraphicEffectManager::ReleaseAll()
+	{
+		for (auto effect : m_pGraphicEffects)
+		{
+			delete effect;
+		}
+
+		if (m_pGraphicEffects.size() == 0) return;
+
+		m_pGraphicEffects.clear();
+		m_pGraphicEffects.shrink_to_fit();
+	}
+
+	void GraphicEffectManager::Update()
+	{
+		ReleaseEndEffect();
+
+		for (auto effect : m_pGraphicEffects)
+		{
+			effect->Update();
+		}
+	}
+
+	void GraphicEffectManager::Render()
+	{
+		for (auto effect : m_pGraphicEffects)
+		{
+			effect->Render();
+		}
+	}
+
+	void GraphicEffectManager::ReleaseEndEffect()
+	{
+		bool isReleased = false;
+
+		for (int i = static_cast<int>(m_pGraphicEffects.size() - 1); i >= 0; --i)
+		{
+			if (!m_pGraphicEffects[i]->IsEnd()) continue;
+
+			delete m_pGraphicEffects[i];
+
+			// 現在のiが指し示している場所の削除
+			m_pGraphicEffects.erase(m_pGraphicEffects.begin() + i);
+
+			isReleased = true;
+		}
+
+		if (!isReleased) return;
+
+		m_pGraphicEffects.shrink_to_fit();
+	}
+}
---
+++ GameFramework/GameFramework/Graphic/GraphicEffectManager/PartScheduler/PartScheduler.cpp
@@ -0,0 +1,70 @@
+﻿#include "PartScheduler.h"
+
+namespace gameframework
+{
+	PartScheduler::PartScheduler()
+	{
+
+	}
+
+	PartScheduler::~PartScheduler()
+	{
+	
+	}
+
+	void PartScheduler::Update()
+	{
+		UpdateCanRunning();
+
+		for (auto& nodeData : m_nodeDatas)
+		{
+			if (IsPartEnd(nodeData)) continue;
+
+			nodeData.m_pT->Update();
+		}
+	}
+
+	void PartScheduler::Render(bool isAdditionBlendMode)
+	{
+		for (auto& nodeData : m_nodeDatas)
+		{
+			if (IsPartEnd(nodeData)) continue;
+
+			nodeData.m_pT->Render(isAdditionBlendMode);
+		}
+	}
+
+	bool PartScheduler::IsEnd()
+	{
+		for (auto& nodeData : m_nodeDatas)
+		{
+			//ノードが終了していないかつ動作時間がまだあるということは完全に終了はしていない
+			if (!nodeData.m_pT->IsEnd() && nodeData.m_runableRemainingFrame != 0) return false;
+		}
+
+		return true;
+	}
+
+	bool PartScheduler::IsParentNodeEnded(NodeData& nodeData)
+	{
+		//親ノードが終了しているかチェック
+		int index = nodeData.m_parentNodeIndex;
+
+		//親ノードがある
+		if (index != NodeData::DEFAULT_START)
+		{
+			//親ノードが終了していない
+			if (!m_nodeDatas[index].m_pT->IsEnd() &&
+				m_nodeDatas[index].m_runableRemainingFrame != 0) return false;
+		}
+
+		return true;
+	}
+
+	bool PartScheduler::IsPartEnd(const NodeData& nodeData)
+	{
+		if (!nodeData.m_canRunning || nodeData.m_pT->IsEnd()) return true;
+
+		return false;
+	}
+}
